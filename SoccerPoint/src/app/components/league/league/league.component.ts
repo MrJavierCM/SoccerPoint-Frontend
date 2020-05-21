8cb2724ebb97bf426429150c942b492f95afb6ce
MrJavierCM <jacamia@inf.upv.es>
2020-05-21 12:31:22 +0200

    Funcionalidades principales del tab de la liga (Clasificación y jornadas)

--- SoccerPoint/src/app/components/league/league/league.component.ts
+++ SoccerPoint/src/app/components/league/league/league.component.ts
@@ -7,18 +7,48 @@ import { FootballService } from 'src/app/services/football.service';
   styleUrls: ['./league.component.scss'],
 })
 export class LeagueComponent implements OnInit {
-  protected standing: string[] = [];
+  protected table: any;
+  protected fixture: any;
+  protected currentFixture: any;
+  protected standingChecked: boolean = false;
+  protected fixtureChecked: boolean = false;
+  protected currentChecked: boolean = false;
+  protected rounds: number [] = [];
+  protected roundFixture: number;
 
   constructor(protected footballService: FootballService) { }
 
-  ngOnInit() {}
-
-  getStanding(){
-    this.footballService.getStanding();
+  ngOnInit() {
+    for(var i = 1; i < 39; i++){
+      this.rounds[i-1]=i;
+    }
   }
 
-  test(){
-    this.footballService.getCurrentFixture();
+  async getStanding(){
+    this.fixtureChecked = false;
+    this.currentChecked = false;
+    var resp = await this.footballService.getStanding();
+    var respJSON = await resp.json();
+    this.table = respJSON.api.standings["0"];
+    this.standingChecked = true;
   }
 
+  async getFixtureByRound(){
+    this.currentChecked = false;
+    this.standingChecked = false;
+    console.log("Número de la jornada: " + this.roundFixture)
+    var resp = await this.footballService.getFixtureByNumber(this.roundFixture);
+    var respJSON = await resp.json();
+    this.fixture = respJSON.api.fixtures;
+    this.fixtureChecked = true;
+  }
+
+  async getCurrentFixture(){
+    this.standingChecked = false;
+    this.fixtureChecked = false;
+    var resp = await this.footballService.getCurrentFixture();
+    var respJSON = await resp.json();
+    this.currentFixture = respJSON.api.fixtures;
+    this.currentChecked = true;
+  }
 }
--- SoccerPoint/src/app/services/football.service.ts
+++ SoccerPoint/src/app/services/football.service.ts
@@ -4,38 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FootballService {
-  protected teams: String[] = [];
+  protected teams: string[] = [];
   constructor() { }
 
   async getCurrentFixture(){
-    var resp = await fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/rounds/775/current", {
-	    "method": "GET",
-	    "headers": {
-		    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "3199bd427bmsh4a82e3f96850bddp18508cjsn1f307202f708"
-	    }
+    var resp = await fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/775/next/10", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+        "x-rapidapi-key": "3199bd427bmsh4a82e3f96850bddp18508cjsn1f307202f708"
+      }
     })
-    
-    var respJSON = await resp.json();
-    console.log(respJSON);
+    return await resp;
   }
 
-  getFixtureByNumber(number){
-    const startURL = "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/775/Regular_Season_-_"
-    const finalURL = "?timezone=Europe%252FMadrid"
-    fetch(startURL+number+finalURL, {
-	    "method": "GET",
-	    "headers": {
-	  	   "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "3199bd427bmsh4a82e3f96850bddp18508cjsn1f307202f708"
-	    }
+  async getFixtureByNumber(number){
+    const startURL = "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/775/Regular_Season_-_";
+    const finalURL = "?timezone=Europe%252FLondon";
+    var url = startURL+number;
+    console.log("URL: " + url);
+    var resp = await fetch(url, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+        "x-rapidapi-key": "3199bd427bmsh4a82e3f96850bddp18508cjsn1f307202f708"
+      }
     })
-    .then(response => {
-	   console.log(response);
-    })
-    .catch(err => {
-	  console.log(err);
-    });
+    return await resp;
   }
 
   async getStanding(){
@@ -46,14 +41,14 @@ export class FootballService {
 		    "x-rapidapi-key": "3199bd427bmsh4a82e3f96850bddp18508cjsn1f307202f708"
 	    }
     })
-    var respJSON = await resp.json();
-    var table = respJSON.api.standings["0"]
-    const start = async () => {
-      await table.forEach(team => {
-        this.teams.push(team["teamName"])
-     });
-     return this.teams;
-    }
-    start();
+    return await resp;
+    // var table = respJSON.api.standings["0"]
+    // const start = async () => {
+    //   await table.forEach(team => {
+    //     this.teams.push(team["teamName"])
+    //  });
+    //  return this.teams;
+    // }
+    // return start();
   }
 }
