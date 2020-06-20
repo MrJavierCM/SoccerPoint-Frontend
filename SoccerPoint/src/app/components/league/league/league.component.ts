612ef4f672974f1f8ae403f063f84de242affb76
MrJavierCM <jacamia@inf.upv.es>
2020-06-20 12:54:27 +0200

    Cambiado formato de los botones de la liga

--- SoccerPoint/src/app/components/league/league/league.component.ts
+++ SoccerPoint/src/app/components/league/league/league.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FootballService } from 'src/app/services/football.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-league',
@@ -15,8 +16,12 @@ export class LeagueComponent implements OnInit {
   protected currentChecked: boolean = false;
   protected rounds: number [] = [];
   protected roundFixture: number;
+  protected loadingInfo;
 
-  constructor(protected footballService: FootballService) { }
+  constructor(
+    protected footballService: FootballService,
+    protected loadingController: LoadingController
+    ) { }
 
   ngOnInit() {
     for(var i = 1; i < 39; i++){
@@ -27,28 +32,65 @@ export class LeagueComponent implements OnInit {
   async getStanding(){
     this.fixtureChecked = false;
     this.currentChecked = false;
+    this.showLoadingInformation()
     var resp = await this.footballService.getStanding();
     var respJSON = await resp.json();
-    this.table = respJSON.api.standings["0"];
+    this.table = respJSON.api.standings["0"];    
+    this.stopLoadingInformation();
     this.standingChecked = true;
   }
 
   async getFixtureByRound(){
     this.currentChecked = false;
-    this.standingChecked = false;
-    console.log("Número de la jornada: " + this.roundFixture)
+    this.standingChecked = false;    
+    this.showLoadingInformation();
     var resp = await this.footballService.getFixtureByNumber(this.roundFixture);
     var respJSON = await resp.json();
+    this.stopLoadingInformation();
     this.fixture = respJSON.api.fixtures;
     this.fixtureChecked = true;
   }
 
   async getCurrentFixture(){
     this.standingChecked = false;
-    this.fixtureChecked = false;
+    this.fixtureChecked = false;    
+    this.showLoadingInformation();
     var resp = await this.footballService.getCurrentFixture();
     var respJSON = await resp.json();
+    this.stopLoadingInformation();
     this.currentFixture = respJSON.api.fixtures;
     this.currentChecked = true;
+  }
+
+  segmentChanged(clicked: any){
+    switch (clicked.detail.value){
+      case "currentFixture":{
+        this.getCurrentFixture()
+        break;
+      }
+      case "fixtureByRound":{
+        this.getFixtureByRound();
+        break;
+      }
+      case "standing":{        
+        this.getStanding();
+        break;
+      }
+    }
+  }
+
+  async showLoadingInformation(){
+    this.loadingInfo = await this.loadingController.create({
+      spinner: 'bubbles',
+      message: 'Buscando...',
+    })
+    .then((res)=>{
+      res.present();
+      res.onDidDismiss();
+    })
+  }
+
+  async stopLoadingInformation(){
+    await this.loadingController.dismiss();
   }
 }
